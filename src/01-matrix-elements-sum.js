2fd3c86c44dd11d45ea0f90c4f38fb229bffade2
Jeredas <efioss93@mail.ru>
2021-04-10 12:10:21 +0300

    feat : add solution for tasks 1 , 7 ,8

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,21 +1,14 @@
-/**
- * Given matrix, a rectangular matrix of integers,
- * just add up all the values that don't appear below a "0".
- *
- * @param {Array<Array>} matrix
- * @return {Number}
- *
- * @example
- * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
- * ]
- *
- * The result should be 9
- */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === 0) {
+        break;
+      }
+      result.push(matrix[j][i]);
+    }
+  }
+  return result.reduce((a, b) => a + b, 0);
 }
 
 module.exports = getMatrixElementsSum;
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,20 +1,15 @@
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const list = {};
+  const result = [];
+  names.forEach((name) => {
+    if (result.includes(name)) {
+      list[name] = list[name] ? list[name] + 1 : 1;
+      result.push(`${name}(${list[name]})`);
+    } else {
+      result.push(name);
+    }
+  });
+  return (result);
 }
 
 module.exports = renameFiles;
