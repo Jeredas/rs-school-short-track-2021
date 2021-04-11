d825227a93c1d6bf4439d949163d23ef8072ff09
Jeredas <efioss93@mail.ru>
2021-04-11 11:17:37 +0300

    feat : add colution for task 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -1,27 +1,14 @@
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const result = {};
+  domains.forEach((domain) => {
+    const domainNames = domain.split('.').reverse();
+    let sum = '';
+    domainNames.forEach((name) => {
+      sum = `${sum}.${name}`;
+      result[sum] = !result[sum] ? 1 : result[sum] + 1;
+    });
+  });
+  return result;
 }
 
 module.exports = getDNSStats;
